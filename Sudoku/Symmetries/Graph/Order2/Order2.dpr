1b3ccf6c1eac8bc6deb3d3ad6667d4b20d127fae
Martin Harvey <martin_c_harvey@hotmail.com>
2021-02-27 18:24:15 +0000

    Revert "Sudoku: Remove inefficient isomorphism generation method, to be replaced"
    This reverts commit a67490a966f3c6a8dba42e7435a1b6688a52ff29.

---
+++ Sudoku/Symmetries/Graph/Order2/Order2.dpr
@@ -0,0 +1,27 @@
+program Order2;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  SharedSymmetries in '..\..\Shared\SharedSymmetries.pas',
+  SymmetryIterations in '..\..\Shared\SymmetryIterations.pas',
+  Trackables in '..\..\..\..\Common\Tracking\Trackables.pas',
+  BinaryTree in '..\..\..\..\Common\Balanced Tree\BinaryTree.pas',
+  NaiveSymmetries in '..\..\Naive\NaiveSymmetries.pas',
+  GraphSymmetries in '..\GraphSymmetries.pas',
+  DLList in '..\..\..\..\Common\DLList\DLList.pas',
+  ExactCover in '..\..\..\..\Common\ExactCover\ExactCover.pas',
+  SparseMatrix in '..\..\..\..\Common\SparseMatrix\SparseMatrix.pas';
+
+begin
+  try
+    //Test2;
+    Test3;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
