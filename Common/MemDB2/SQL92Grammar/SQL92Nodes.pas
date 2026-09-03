b53086e7a28e7b18fc20ad04f802b369eca882f6
Martin Harvey <martin_c_harvey@hotmail.com>
2026-09-03 20:20:41 +0100

    SQL92Grammar: Update. WIP. Literals, Idents, Modules.

--- Common/MemDB2/SQL92Grammar/SQL92Nodes.pas
+++ Common/MemDB2/SQL92Grammar/SQL92Nodes.pas
@@ -26,12 +26,20 @@ IN THE SOFTWARE.
 
 interface
 
-uses CommonNodes, SysUtils;
+uses CommonNodes, SysUtils, Trackables;
+
+{ N.B All this written with "bare minimum necessary" at the moment.
+  I don't yet know how the various subsystems / structures will pan out,
+  so, I'm just trying to keep things with a minimum of compexity to start
+  with. i.e. we have enumerated types for "what sort of", when we
+  expect class overrides / specialisation may handle that later on. }
 
 type
+
   TSQLSynNode = class(TCommonNode)
   public
     constructor Create; virtual;
+    constructor CreateWithTracker(Tracker: TTracker); virtual;
     destructor Destroy; override;
 
     function FirstChild: TSQLSynNode; inline;
@@ -45,11 +53,20 @@ type
     procedure InsertAfterSibling(Dest: TSQLSynNode);
     procedure InsertBeforeSibling(Dest: TSQLSynNode);
 
+    procedure RemoveFromTree;
+
     procedure ValidateAST;
     procedure ValidateSelf; virtual;
   end;
 
   TSQLSynNodeClass = class of TSQLSynNode;
+
+  //Just a temporary thing for the parser where ordering is required.
+  //Rely on tracking and assertions to make sure this is not misused.
+  TTempTpl = class(TSqlSynNode)
+  public
+    T1, T2, T3, T4: TSQLSynNode;
+  end;
 
   ESQLSynException = class(Exception)
   private
@@ -60,18 +77,32 @@ type
     property N2: TSQLSynNode read FN2;
   end;
 
+  TSQLSynExpr = class(TSqlSynNode)
+    //Todo - lots of things on expr return type, evaluatability
+    //folding, who knows what else.
+  end;
+
   TSQLSynLiteralType = (
+    //Basic, simple string represented literals.
     sltUnsInt,
     sltInt,
     sltExactNumeric,
     sltApproxNumeric,
+    sltSignedExactNumeric,
+    sltSignedApproxNumeric,
     sltNatString,
     sltString,
     sltBitString,
-    sltHexString
+    sltHexString,
+    sltIntervalString,
+    sltDate,
+    sltTime,
+    sltTimestamp,
+    //Compound literals
+    sltInterval
   );
 
-  TSQLSynLiteral = class(TSQLSynNode)
+  TSQLSynLiteral = class(TSQlSynExpr)
   private
     FText: string;
     FLitType: TSQLSynLiteralType;
@@ -80,11 +111,143 @@ type
     property LitType: TSQLSynLiteralType read FLitType write FLitType;
   end;
 
-  TSQLIdentifier = class(TSqlSynNode)
+  TSQLSynIntervalStringType = (
+    istPlainInt,
+    istYearMonth,
+    istDayTime1,
+    istDayTime2,
+    istDayTime3,
+    istTime1,
+    istTime2,
+    istTime3);
+
+  TSQLSynIntervalStringLiteral = class(TSQLSynLiteral)
   private
-    FName: string;
+    FIntervalStringType: TSQLSynIntervalStringType;
   public
-    property Name: string read FName write FName;
+    property IntervalStringType: TSQLSynIntervalStringType
+      read FIntervalStringType write FIntervalStringType;
+  end;
+
+  TSQLSynIntervalQualifier = class;
+
+  TSQLSynIntervalLiteral = class(TSQLSynLiteral)
+  private
+    FInterval: TSQLSynIntervalStringLiteral;
+    FQualifier: TSQLSynIntervalQualifier;
+    FNegated: boolean;
+  public
+    property Negated: boolean read FNegated write FNegated;
+    property Interval: TSQLSynIntervalStringLiteral read FInterval write FInterval;
+    property Qualifier: TSQLSynIntervalQualifier read FQualifier write FQualifier;
+  end;
+
+  TSQLSynIdent = class(TSqlSynNode)
+  private
+    FIdentName: string;
+    FWildcard: boolean;
+  public
+    property IdentName: string read FIdentName write FIdentName;
+    property Wildcard: boolean read FWildcard write FWildCard;
+  end;
+
+  //Expect some level of coercion allowed between literal types, and
+  //general types, but we'll get there in a bit.
+  TSQLGeneralType = (
+    sgtExactNumeric,
+    sgtApproxNumeric,
+    sgtNatString,
+    sgtString,
+    sgtBitString,
+    sgtHexString,
+    sgtDate,
+    sgtTime,
+    sgtTimestamp,
+    sgtInterval,
+    sgtRelation { Returns something relation-like }
+  );
+
+  TSQLSynType = class(TSqlSynNode)
+  private
+    FGeneralType: TSQLGeneralType;
+  public
+    property GeneralType: TSQLGeneralType read FGeneralType write FGeneralType;
+  end;
+
+  TSQLSynIntervalType = class(TSQLSynType)
+  private
+    FQualifier: TSQLSynIntervalQualifier;
+  public
+    property Qualifier: TSQLSynIntervalQualifier read FQualifier write FQualifier;
+  end;
+
+  TSQLSynQualField = (
+    sqfSecond,
+    sqfMinute,
+    sqfHour,
+    sqfDay,
+    sqfMonth,
+    sqfYear
+  );
+
+  TSQLSynIntervalQualifier = class(TSQLSynNode)
+  private
+    FStart, FEnd: TSQLSynQualField;
+  public
+    property Start: TSQLSynQualField read FStart write FStart;
+    property _End: TSQLSynQualField read FEnd write FEnd;
+  end;
+
+  { Built in functions / constants which are effectively exprs. }
+  TSQLSynBuiltInType = (
+    sftCurrentDate,
+    sftCurrentTime,
+    sftCurrentTimestamp,
+    sftUser,
+    sftCurrentUser,
+    sftSessionUser,
+    sftSystemUser,
+    sftNull
+  );
+
+  TSqlSynBuiltin = class(TSQlSynExpr)
+  private
+    FBuiltInType: TSqlSynBuiltinType;
+  public
+    property BuiltInType: TSQLSynBuiltinType read FBuiltInType write FBuiltInType;
+  end;
+
+  //TODO - I'm going to worry about identifier resolution
+  //and namespaces a bit later, because there are many and varied ways of introducing them.
+  TSQLSynStructuralType = (
+    sstModule
+  );
+
+  TSQLSynStructural = class(TSQLSynNode)
+  private
+    FStructuralType: TSQLSynStructuralType;
+  public
+    property StructuralType:TSQLSynStructuralType read FStructuralType write FStructuralType;
+  end;
+
+  TSQLSynNamedStructural = class(TSQLSynStructural)
+  private
+    FName: TSqlSynIdent;
+    //TODO - First and last pointers for contained list of stuff?
+  public
+    property Name:TSqlSynIdent read FName write FName;
+  end;
+
+  TSQLSynModule = class(TSQLSynNamedStructural)
+  private
+    FLanguage, FSchema, FAuthorization: TSQLSynIdent;
+    FFirstContents: TSQLSynNode;
+  public
+    property Language: TSqlSynIdent read FLanguage write FLanguage;
+    property Schema: TSqlSynIdent read FSchema write FSchema;
+    property Authorization: TSqlSynIdent read FAuthorization write FAuthorization;
+    //Includes both temp tbl decls and contents.
+    property FirstContents: TSqlSynNode read FFirstContents write FFirstContents;
   end;
 
 implementation
@@ -109,6 +272,11 @@ end;
 { TSQLSynNode }
 
 constructor TSQLSynNode.Create;
+begin
+  inherited;
+end;
+
+constructor TSQLSynNode.CreateWithTracker(Tracker: TTracker);
 begin
   inherited;
 end;
@@ -144,24 +312,50 @@ procedure TSQLSynNode.InsertHeadChild(Dest: TSQLSynNode);
 begin
   if not (Assigned(Self) and Assigned(Dest)) then
     raise ESQLSynException.Create(S_INSERTING_NIL_CHILD);
+  Assert(DLItemIsEmpty(@Dest.FSiblingListEntry));
+  Assert(not Assigned(Dest.FContainerNode));
+  DLListInsertHead(@self.FContainedListHead, @Dest.FSiblingListEntry);
+  Dest.FContainerNode := self;
 end;
 
 procedure TSQLSynNode.InsertTailChild(Dest: TSQLSynNode);
 begin
   if not (Assigned(Self) and Assigned(Dest)) then
     raise ESQLSynException.Create(S_INSERTING_NIL_CHILD);
+  Assert(DLItemIsEmpty(@Dest.FSiblingListEntry));
+  Assert(not Assigned(Dest.FContainerNode));
+  DLListInsertTail(@self.FContainedListHead, @Dest.FSiblingListEntry);
+  Dest.FContainerNode := self;
 end;
 
 procedure TSQLSynNode.InsertAfterSibling(Dest: TSQLSynNode);
 begin
   if not (Assigned(Self) and Assigned(Dest)) then
     raise ESQLSynException.Create(S_INSERTING_NIL_CHILD);
+  Assert(DLItemIsEmpty(@Dest.FSiblingListEntry));
+  Assert(not Assigned(Dest.FContainerNode));
+  DLItemInsertAfter(@self.FSiblingListEntry, @dest.FSiblingListEntry);
+  Dest.FContainerNode := self.FContainerNode;
 end;
 
 procedure TSQLSynNode.InsertBeforeSibling(Dest: TSQLSynNode);
 begin
   if not (Assigned(Self) and Assigned(Dest)) then
     raise ESQLSynException.Create(S_INSERTING_NIL_CHILD);
+  Assert(DLItemIsEmpty(@Dest.FSiblingListEntry));
+  Assert(not Assigned(Dest.FContainerNode));
+  DLItemInsertBefore(@self.FSiblingListEntry, @dest.FSiblingListEntry);
+  Dest.FContainerNode := self.FContainerNode;
+end;
+
+procedure TSQLSynNode.RemoveFromTree;
+begin
+  Assert(DLItemIsEmpty(@FSiblingListEntry) = not Assigned(FContainerNode));
+  if Assigned(FContainerNode) then
+  begin
+    FContainerNode := nil;
+    DLListRemoveObj(@FSiblingListEntry);
+  end;
 end;
 
 procedure TSQLSynNode.ValidateAST;
@@ -180,5 +374,6 @@ end;
 procedure TSQLSynNode.ValidateSelf;
 begin
 end;
+
 
 end.
